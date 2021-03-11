50df42ffbbbe0eb451b286ba80f1b9baae6750c4
yuzhanglong <yuzl1123@163.com>
2021-03-11 13:42:40 +0800

    :arrow_up: chore(examples): update example's deps and configurations

--- examples/vscode/.eslintrc.js
+++ examples/vscode/.eslintrc.js
@@ -16,4 +16,5 @@ module.exports = {
       }
     }
   ],
+  root: true
 }
--- examples/webstorm/.eslintrc.js
+++ examples/webstorm/.eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
-  extends: ['plugin:@lint-md/recommend'],
+  extends: [
+    'eslint:recommended'
+  ],
+  overrides: [
+    {
+      files: ['*.md'],
+      parser: '@lint-md/eslint-plugin/src/parser',
+      extends: ['plugin:@lint-md/recommend'],
+      rules: {
+        // 在这里覆盖已有的 rules
+        '@lint-md/no-long-code': [2, {
+          "length": 100,
+          "exclude": []
+        }]
+      }
+    }
+  ],
   root: true
 }
